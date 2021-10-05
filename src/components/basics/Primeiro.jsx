b6b24769c3e37f51385bc67550404820b37a9d9d
Jose Mauro <josemauromani@gmail.com>
2021-10-05 13:19:58 -0300

    initial commit

---
+++ src/components/basics/Primeiro.jsx
@@ -0,0 +1,9 @@
+export default function Primeiro() {
+    const msg = 'Seja bem vindo';
+    return (
+        <div>
+            <h1>Primeiro Componente</h1>
+            <h3>{ msg }</h3>
+        </div>
+    );
+}
